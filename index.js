e4bc531080a684a4658ae42e78f5c8038a356117
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-03-22 01:04:16 +0100

    Add: Added Toggle functionalities

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-var output = document.getElementById("output-go");
+let toggleValue = true;
+const distinctStyleButtons = document.querySelectorAll(".distinct-style");
+
+const originalClassNames = Array.from(distinctStyleButtons).map(
+  (button) => button.className
+);
+
+document.getElementById("togglebtn").addEventListener("click", function () {
+  toggleValue = !toggleValue;
+  console.log("Toggle value:", toggleValue);
+
+  distinctStyleButtons.forEach((button, index) => {
+    if (toggleValue === false) {
+      button.className = "toggled-value";
+    } else {
+      button.className = originalClassNames[index];
+    }
+  });
+});
 
 function display(num) {
   output.value += num;
@@ -6,6 +24,7 @@ function display(num) {
 function Clear() {
   return (output.value = "");
 }
+
 function calculate() {
   try {
     return (output.value = eval(output.value));
